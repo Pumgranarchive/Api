482eb46cc4104655274097d14ee4ac47b3b80475
ArnaudParant <ard.parant@gmail.com>
2015-11-06 13:43:44 +0100

    [Configuration] Added https

--- redirect.sh
+++ redirect.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8081
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 4443
