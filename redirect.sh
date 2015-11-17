6e6e81b72b36691f0f9881d5bf5f97f04f9f3ee5
ArnaudParant <ard.parant@gmail.com>
2015-11-17 22:52:00 +0100

    Fix redirect.sh

--- redirect.sh
+++ redirect.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8081
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j REDIRECT --to-port 4443
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport http -j REDIRECT --to-port 8081
+iptables -t nat -A PREROUTING -i eth0 -p tcp --dport https -j REDIRECT --to-port 4443
